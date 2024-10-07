0795802314b2e19cbf99162fdb3087fa4660eabe
Marko Cavrak <96372327+Cavrak396@users.noreply.github.com>
2024-10-07 14:08:10 +0200

    [Added] implementing header for desktop

---
+++ greenshop-web/src/components/button/Button.tsx
@@ -0,0 +1,16 @@
+import { ReactNode, ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  className?: string;
+}
+
+function Button({ children, className, ...rest }: ButtonProps) {
+  return (
+    <button type="button" className={className} {...rest}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
---
+++ greenshop-web/src/components/header/HeaderUserToolbar.tsx
@@ -0,0 +1,41 @@
+import Button from "../button/Button";
+import magnifier from "../../assets/magnifier.png";
+import cart from "../../assets/cart.png";
+import login from "../../assets/login-icon.png";
+
+interface ToolbarItem {
+  id: number;
+  src: string;
+  alt: string;
+}
+
+const toolbarItems: ToolbarItem[] = [
+  { id: 1, src: magnifier, alt: "Search" },
+  { id: 2, src: cart, alt: "Cart" },
+];
+
+function HeaderUserToolbar() {
+  return (
+    <ul className="header__usertoolbar-list">
+      {toolbarItems.map((item) => (
+        <li className="header__usertoolbar-item" key={item.id}>
+          <Button className="header__usertoolbar-button">
+            <img
+              src={item.src}
+              alt={item.alt}
+              className="header__usertoolbar-image"
+            />
+          </Button>
+        </li>
+      ))}
+      <li className="header__usertoolbar-item">
+        <Button className="header__usertoolbar-button header__usertoolbar-button--login">
+          <img src={login} alt="Login icon" />
+          Login
+        </Button>
+      </li>
+    </ul>
+  );
+}
+
+export default HeaderUserToolbar;
