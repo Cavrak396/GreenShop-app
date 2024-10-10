463bd08c6cbae730565e732faecf71d6e5ba48bc
usuibg <usuit173@gmail.com>
2024-10-10 17:25:05 +0200

    test

--- greenshop-api/Controllers/WeatherForecastController.cs
+++ greenshop-api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace greenshop_api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "ssssssssssssssssssssssssssssssssssssssssssss"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
