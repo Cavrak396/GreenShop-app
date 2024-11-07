10e21362ac0ae883e0ff5fb7f082d08486a9b4ec
Marko Cavrak <96372327+Cavrak396@users.noreply.github.com>
2024-11-07 16:04:22 +0100

    [Added] implementing new homepage structure

--- greenshop-web/src/App.tsx
+++ greenshop-web/src/App.tsx
@@ -1,23 +1,15 @@
 import "./App.css";
 import Header from "./components/header/Header";
-import Banner from "./components/banner/Banner";
-import HomePageShop from "./components/homepage-shop/HomePageShop";
-import About from "./components/about/About";
-import Blogs from "./components/homepage-blogs/Blogs";
 import Footer from "./components/footer/Footer";
 import { CartProvider } from "./context/CartContext";
-import UserBar from "./components/responsive-userbar/UserBar";
+import HomePage from "./components/pages/HomePage";
 
 function App() {
   return (
     <CartProvider>
       <Header />
-      <Banner />
-      <HomePageShop />
-      <About />
-      <Blogs />
+      <HomePage />
       <Footer />
-      <UserBar />
     </CartProvider>
   );
 }
---
+++ greenshop-web/src/components/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import Banner from "../banner/Banner";
+import HomePageShop from "../homepage-shop/HomePageShop";
+import About from "../about/About";
+import Blogs from "../homepage-blogs/Blogs";
+
+function HomePage() {
+  return (
+    <>
+      <Banner />
+      <HomePageShop />
+      <About />
+      <Blogs />
+    </>
+  );
+}
+
+export default HomePage;
