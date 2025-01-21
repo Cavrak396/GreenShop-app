f231ff35d4e24882d31dbf7e66613b20fe128426
lilacturt <jurisic.ana.official@gmail.com>
2025-01-21 10:01:54 +0100

    [Fixed] Filters for JWT

---
+++ greenshop-api/Filters/ActionFilters/Review_ActionFilters/Review_ValidateJwtTokenFilterAttribute.cs
@@ -0,0 +1,39 @@
+﻿using greenshop_api.Authority;
+using greenshop_api.Services;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace greenshop_api.Filters.ActionFilters.Review_ActionFilters
+{
+    public class Review_ValidateJwtTokenFilterAttribute : IAsyncActionFilter
+    {
+        private readonly IUserRepository repository;
+        private readonly JwtService jwtService;
+
+        public Review_ValidateJwtTokenFilterAttribute(IUserRepository repository, JwtService jwtService)
+        {
+            this.repository = repository;
+            this.jwtService = jwtService;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var jwt = context.HttpContext.Request.Cookies["jwt"];
+
+            if (!string.IsNullOrEmpty(jwt))
+            {
+                try
+                {
+                    var token = jwtService.Verify(jwt);
+                    var userId = token.Issuer.ToString();
+                    var user = await repository.GetUserByIdAsync(userId);
+                }
+                catch (Exception)
+                {
+                    ModelErrors.AddUnauthorizedActionModelError(context, "User", "Unauthenticated user.");
+                    return;
+                }
+            }
+
+            await next();
+        }
+    }
+}
